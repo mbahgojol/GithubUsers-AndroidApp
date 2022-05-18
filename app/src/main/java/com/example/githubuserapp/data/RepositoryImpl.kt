c599594df2f404dfadb8983ec64b4d09f106835c
knalbdev <profghozimahdi@gmail.com>
2022-05-18 10:56:00 +0700

    test

--- app/src/main/java/com/example/githubuserapp/data/RepositoryImpl.kt
+++ app/src/main/java/com/example/githubuserapp/data/RepositoryImpl.kt
@@ -10,6 +10,7 @@ import com.example.githubuserapp.utils.ResultState
 import com.example.githubuserapp.utils.flowApi
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.withContext
--- app/src/main/java/com/example/githubuserapp/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/githubuserapp/ui/main/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.githubuserapp.ui.main
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
+import android.os.Handler
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
@@ -56,6 +57,15 @@ class MainActivity : AppCompatActivity() {
                         else -> false
                     }
             })
+        } else {
+            supportActionBar?.hide()
+            binding.content.isVisible = false
+            binding.isplash.splashLayout.isVisible = true
+            Handler(mainLooper).postDelayed({
+                supportActionBar?.show()
+                binding.content.isVisible = true
+                binding.isplash.splashLayout.isVisible = false
+            }, 3000)
         }
 
         viewModel.getThemeSettings().observe(this) {
